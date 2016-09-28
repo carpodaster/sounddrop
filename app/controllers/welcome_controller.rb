a3be9c0c2eb70a6f61fa3f851b7cb0c71287b4e4
Markus Waitl <axlwaii@gmail.com>
2016-09-28 22:29:36 +0200

    redefine map output
    I'm Rick Harrison, and this is my pawn shop. I work here with my old
    man and my son, Big Hoss. Everything in here has a story and a price.
    One thing I've learned after 21 years - you never know what is gonna
    come through that door.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @drops = Drop.none
+    @drops = [Drop.new({title: "ABSOLVENTA", longitude: 13.3900989, latitude: 52.5120181, sc_track: "dummy"})]
   end
 
   def about
