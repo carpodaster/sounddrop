4bbc01d679b9c7fc3e2f8b939b4b828fed7224e7
Robin Neumann <neumanrq@gmail.com>
2016-09-28 18:15:45 +0200

    Fixed the radius is!

--- app/controllers/api/drops_controller.rb
+++ app/controllers/api/drops_controller.rb
@@ -4,13 +4,29 @@ class Api::DropsController < ApplicationController
   DEFAULT_RADIUS = 10
   respond_to :json
 
+  before_action :notify_for_fixed_radius
+
   def index
     if params[:latitude] && params[:longitude]
-      radius = params[:radius].presence || DEFAULT_RADIUS
-      places_within_radius = Place.near([params[:latitude], params[:longitude]], radius, units:  :km)
+      places_within_radius = Place.near([params[:latitude], params[:longitude]], DEFAULT_RADIUS, units:  :km)
       @drops = Drop.where(place_id: places_within_radius.map(&:id))
     else
       @drops = Drop.all
     end
   end
+
+  private
+
+  def notify_for_fixed_radius
+    if params[:radius]
+      render json: {
+        accepted_radius: 10,
+        queried_radius: params[:radius],
+        comments: {
+          yoda: "Not allowed it is in this version of sounddrop. Fixed the radius is. In your cleverness we trust, young padawans. Always pass on what you have learned."
+        }
+      }
+    end
+  end
+
 end
