9aecefe1f2c3ca44980397f05f23b9b78190e8e9
Carsten Zimmermann <cz@aegisnet.de>
2016-09-29 11:47:02 +0200

    Use the welcome track

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @drops = [Drop.new({title: "ABSOLVENTA", longitude: 13.3900989, latitude: 52.5120181, sc_track: "dummy"})]
+    @drops = [Drop.find(234882)] # welcome track
   end
 
   def about
